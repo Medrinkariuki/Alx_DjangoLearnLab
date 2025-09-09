456a97c36016457edf290ea71b6cc602cac4d068
Medrin Kariuki <medrinkariuki@gmail.com>
2025-09-09 11:47:14 +0300

    Fix query_samples.py to pass all checkers

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,30 @@
-from .models import Author, Book, Library, Librarian
+# query_samples.py
+from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
-
-# List all books in a library
-def books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
+    """
+    Returns a queryset of all books written by the author with the given name.
+    """
+    # Get the author object
+    author = Author.objects.get(name=author_name)
+    # Return all books that have this author
+    return Book.objects.filter(author=author)
 
 # Retrieve the librarian for a library
 def librarian_for_library(library_name):
+    """
+    Returns the librarian assigned to the library with the given name.
+    """
+    # Get the library object
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    # Return the librarian assigned to this library
+    return Librarian.objects.get(library=library)
+
+# Optional: example usage for testing
+if __name__ == "__main__":
+    author_name = "J.K. Rowling"
+    library_name = "Central Library"
+
+    print("Books by author:", books_by_author(author_name))
+    print("Librarian for library:", librarian_for_library(library_name))
