fd3137258e3bd3928d8bf60b35c3c4d0d4752f95
Medrin Kariuki <medrinkariuki@gmail.com>
2025-09-09 10:20:36 +0300

    Add relationship_app models and query samples

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,28 +1,16 @@
-from django.db import models
+from .models import Author, Book, Library, Librarian
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
+def query_books_by_author(author_name):
+    # Get the author object
+    author = Author.objects.get(name=author_name)
+    # Get all books written by that author
+    books = Book.objects.filter(author=author)
+    return books
 
-    def __str__(self):
-        return self.name
+def list_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
 
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.librarian
