1e56598e460581229d985886b9b401c0f13ae35f
Medrin Kariuki <medrinkariuki@gmail.com>
2025-09-10 10:22:35 +0300

    Added FBV book list, CBV library detail, templates, and URLs

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
-from .models import Book  # make sure this points to your Book model
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
 
-def list_books(request):
-    # Get all books
+# Function-based view (simple list of book titles and authors)
+def book_list(request):
     books = Book.objects.all()
-    
-    # Render the template at relationship_app/list_books.html
-    return render(request, "relationship_app/list_books.html", {"books": books})
+    output = ", ".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(output)
+
+# Class-based view (library detail with its books)
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["books"] = self.object.books.all()  # assuming related_name="books"
+        return context
