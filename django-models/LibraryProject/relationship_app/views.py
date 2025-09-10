4ab68ad2c897efdfa0088532d1e9c975bb268569
Medrin Kariuki <medrinkariuki@gmail.com>
2025-09-10 10:58:01 +0300

    Linked function-based and class-based views in urls.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
-def book_list(request):
+def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
