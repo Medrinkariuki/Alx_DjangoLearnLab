79a556ffa2361d2e522edf820d7a63999a0df488
Wambo <wambuimuthima1@gmail.com>
2025-09-19 13:46:00 +0300

    Update models.py
    Updated models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
