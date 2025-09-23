d58dfcce6648eb1339615bf58a697f09ccbbe45f
Medrin Kariuki <medrinkariuki@gmail.com>
2025-09-23 09:59:33 +0300

    Add new Django REST Framework project api_project with Book model

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from .models import Book
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from .models import Book, CustomUser
+from django.contrib.auth.admin import UserAdmin
+
 
 #Create your models here
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('publication_year',)
     search_fields = ('title', 'author')
@@ -30,3 +34,11 @@ class CustomUserAdmin(UserAdmin):
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
+=======
+    list_display = ['title', 'author', 'published_date', 'isbn']  # remove 'pages' if it doesn't exist
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -10,6 +10,29 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+=======
+from django.contrib.auth.models import AbstractUser
+
+# -------------------------------
+# Custom User Model
+# -------------------------------
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+# -------------------------------
+# Book Model
+# -------------------------------
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField(null=True, blank=True)
+    isbn = models.CharField(max_length=13, null=True, blank=True)
+>>>>>>> a54762f (Add new Django REST Framework project api_project with Book model)
 
     def __str__(self):
         return f"{self.title} ({self.publication_year}) by {self.author}"
