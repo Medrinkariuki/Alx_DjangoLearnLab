74405b181c87c54c6cdd8f727917e9ad13709801
Medrin Kariuki <medrinkariuki@gmail.com>
2025-10-13 11:40:43 +0300

    Fix serializers: implement user creation and token generation

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,47 +1,31 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-# ------------------------------
-# User Serializer
-# ------------------------------
 class UserSerializer(serializers.ModelSerializer):
-    followers = serializers.PrimaryKeyRelatedField(
-        many=True,
-        read_only=True
-    )
-    profile_picture = serializers.ImageField(read_only=True)
-
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
-
-# ------------------------------
-# Registration Serializer
-# ------------------------------
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2', 'bio', 'profile_picture']
+        fields = ['username', 'email', 'password', 'password2']
 
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
 
     def create(self, validated_data):
+        # Remove password2 because the User model doesn't have it
         validated_data.pop('password2')
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None)
-        )
+        user = get_user_model().objects.create_user(**validated_data)
+        # Create an auth token for the new user
+        Token.objects.create(user=user)
         return user
